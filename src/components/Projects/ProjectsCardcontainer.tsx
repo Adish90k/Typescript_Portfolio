ca63fa05428bbf54aba3d39e68383e0adef1694b
adish <adishkerkar@gmail.com>
2025-02-07 20:12:26 +0530

    added github geochat link

--- src/components/Projects/ProjectsCardcontainer.tsx
+++ src/components/Projects/ProjectsCardcontainer.tsx
@@ -39,6 +39,7 @@ const ProjectsCardcontainer:React.FC = () => {
           as="button"
           className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white font-bold"
         >
+          <a href="https://github.com/Adish90k/Geochat_backend"></a>
           Github
         </CardItem>
       </div>
