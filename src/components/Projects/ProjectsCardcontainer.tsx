64e15a08c0f55944ec05216282a053ced34e5f23
adish <adishkerkar@gmail.com>
2025-02-07 20:16:24 +0530

    fixed link error

--- src/components/Projects/ProjectsCardcontainer.tsx
+++ src/components/Projects/ProjectsCardcontainer.tsx
@@ -39,8 +39,8 @@ const ProjectsCardcontainer:React.FC = () => {
           as="button"
           className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white font-bold"
         >
-          <a href="https://github.com/Adish90k/Geochat_backend"></a>
-          Github
+          <a href="https://github.com/Adish90k/Geochat_backend">Github</a>
+     
         </CardItem>
       </div>
     </CardBody>
